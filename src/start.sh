294cc1217150c1178e1d3c80b1f2c7f2a6622468
Igor Mitiukliaev <igor_m@outlook.com>
2025-11-07 17:56:22 +0700

    Refactor start.sh

--- src/start.sh
+++ src/start.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-PROJECT_ROOT=/opt poetry run alembic upgrade head
-# add initial data to tables from SQL scripts
-# PROJECT_ROOT=/opt poetry run python -m db.init_db
+cd /opt
+# apply database migrations
+poetry run alembic upgrade head
+
+
+# poetry run python -m db.init_db
 # start the application 
-# PROJECT_ROOT=/opt poetry run uvicorn main:app --host 0.0.0.0 --port $APP_PORT --log-level 'debug' --workers=1
-PROJECT_ROOT=/opt poetry run python -m main
+# poetry run uvicorn main:app --host 0.0.0.0 --port $APP_PORT --log-level 'debug' --workers=1
+poetry run python -m main
